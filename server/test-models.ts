ccac03fe0a07fbaed1d5e0575c9beb1abe4770f1
T0x1c-Devel0per <juanchistos0102@gmail.com>
2026-04-22 23:22:31 -0500

    Implementando mejoras

--- server/test-models.ts
+++ server/test-models.ts
@@ -13,7 +13,7 @@ async function listModels() {
     
     // El SDK oficial actual no exporta listModels de forma sencilla, pero podemos intentar instanciar el cliente fetch
     const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models?key=' + apiKey);
-    const data = await response.json();
+    const data = await response.json() as any;
     console.log('Modelos disponibles:');
     if (data.models) {
       data.models.forEach((m: any) => {
